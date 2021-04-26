fcb0516ddf8e5a91381f26e1b297c70ce82c1b7b
Pranali Taware <pranalitaware@gmail.com>
2021-04-26 12:08:42 +0530

    add payee

--- Banking-frontend/src/app/add-beneficiary/add-beneficiary.component.ts
+++ Banking-frontend/src/app/add-beneficiary/add-beneficiary.component.ts
@@ -12,34 +12,28 @@ export class AddBeneficiaryComponent implements OnInit {
   addBeneficiary: AddBeneficiary = new AddBeneficiary();
   message: string;
   accounts = JSON.parse(sessionStorage.getItem('accounts'));
-  beneficiaryAccountNo: number;
-  userAccountNo:number;
-  //beneficiaryAccountNo : number; 
 
-
-  //accountNumber: number;
   constructor(private service: CustomerService) { }
 
   ngOnInit(): void {
-    //const body=JSON.stringify(person);
+
+    
+    
     this.accounts = JSON.parse(sessionStorage.getItem('accounts'));
     console.log(this.accounts);
-    this.addBeneficiary.compoundKey.userAccountNo=this.accounts[0];
+    
+    //console.log(this.addBeneficiary.compoundKey.userAccountNo);
 
   }
 
   add(){
     //this.addBeneficiary.userAccountNo = parseInt(sessionStorage.getItem('accounts'));
     //alert(this.addBeneficiary.nickName);
+    this.addBeneficiary.compoundKey.userAccountNo=this.accounts[0];
     console.log(this.addBeneficiary.compoundKey.userAccountNo);
-    this.service.addBeneficiary(this.addBeneficiary).subscribe(data=>{
-     // const headers = { 'content-type': 'application/json'}
-      //this.addBeneficiary= data;
-      //this.addBeneficiary.compoundKey.beneficiaryAccount.accountNumber = data.addBeneficiary.compoundKey.beneficiaryAccount.accountNumber;
-      //alert(this.addBeneficiary.compoundKey.beneficiaryAccount.accountNumber);
-      console.log(this.addBeneficiary);
-      alert(JSON.stringify(data));
-    })
-  }
+      alert(JSON.stringify(this.addBeneficiary));
+      this.service.addBeneficiary(this.addBeneficiary).subscribe(data => {
 
+      })
+}
 }
